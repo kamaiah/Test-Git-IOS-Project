c9a47e9ab347d64c650eb2e3bd838357031b8a8c
kamaiah <knadavala@gmail.com>
2014-10-16 17:48:19 +0530

    test class added

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Kamaiah Nadavala on 16/10/14.
+//  Copyright (c) 2014 Yodlee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Kamaiah Nadavala on 16/10/14.
+//  Copyright (c) 2014 Yodlee. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
